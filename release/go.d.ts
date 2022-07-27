67948041111228c9c5da53f2d4ee0b61d2c48800
Simon Sarris <simon.sarris@gmail.com>
2022-07-27 14:36:44 -0400

    2.2.14

--- release/go.d.ts
+++ release/go.d.ts
@@ -1,5 +1,5 @@
 /*
- * Type definitions for GoJS v2.2.13
+ * Type definitions for GoJS v2.2.14
  * Project: https://gojs.net
  * Definitions by: Northwoods Software <https://github.com/NorthwoodsSoftware>
  * Definitions: https://github.com/NorthwoodsSoftware/GoJS
