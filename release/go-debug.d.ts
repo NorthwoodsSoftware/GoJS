47b05a7637d976731f375f4eb66c557e68019037
Simon Sarris <simonsarris@users.noreply.github.com>
2023-05-10 15:21:40 -0400

    2.3.7

--- release/go-debug.d.ts
+++ release/go-debug.d.ts
@@ -1,5 +1,5 @@
 /*
- * Type definitions for GoJS v2.3.6
+ * Type definitions for GoJS v2.3.7
  * Project: https://gojs.net
  * Definitions by: Northwoods Software <https://github.com/NorthwoodsSoftware>
  * Definitions: https://github.com/NorthwoodsSoftware/GoJS
