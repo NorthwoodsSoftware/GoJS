91edb4ff3101aa4862265ab30d7759d04351a7d1
Simon Sarris <simonsarris@users.noreply.github.com>
2024-03-15 10:40:25 -0400

    2.3.16

--- release/go.d.ts
+++ release/go.d.ts
@@ -1,5 +1,5 @@
 /*
- * Type definitions for GoJS v2.3.15
+ * Type definitions for GoJS v2.3.16
  * Project: https://gojs.net
  * Definitions by: Northwoods Software <https://github.com/NorthwoodsSoftware>
  * Definitions: https://github.com/NorthwoodsSoftware/GoJS
