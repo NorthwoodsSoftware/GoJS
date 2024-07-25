aafdfc2edc48819ac4fc1490f1b82b6f6d58cf11
Simon Sarris <simonsarris@users.noreply.github.com>
2024-07-25 10:32:28 -0400

    Delete go.js

--- samples/go.js
+++
@@ -1,6 +0,0 @@
-if (window.navigator.userAgent === 'PUPPET') {
-  // used in buildTS/npm-scripts/jest.test.js
-  document.write('<script src="../../out/go.js"></script>');
-} else {
-  document.write('<script src="../../out/go-dev.js"></script>');
-}
